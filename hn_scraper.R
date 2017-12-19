fb307a25f53e8089f1687c3b903c6ff59d7f3180
amrrs <amrrs@outlook.com>
2017-12-19 20:15:38 +0530

    update with dataframe

--- hn_scraper.R
+++ hn_scraper.R
@@ -6,20 +6,22 @@ content <- read_html(url)
 
 #News Title
 
-content %>% html_nodes('a.storylink') %>% html_text()
+title <- content %>% html_nodes('a.storylink') %>% html_text()
 
 #News Link Domain
 
-content %>% html_nodes('span.sitestr') %>% html_text()
+link_domain <- content %>% html_nodes('span.sitestr') %>% html_text()
 
 #Link Score / Upvote
 
-content %>% html_nodes('span.score') %>% html_text()
+score <- content %>% html_nodes('span.score') %>% html_text()
 
 #Link Age (submission time)
 
-content %>% html_nodes('span.age') %>% html_text()
+age <- content %>% html_nodes('span.age') %>% html_text()
 
+#Final Dataframe
 
+df <- data.frame(title = title, link_domain = link_domain, score = score, age = age)
 #Naive way of extracting the entire page content with this table
 #tb <- content %>% html_node('table.itemlist') %>% html_text()
